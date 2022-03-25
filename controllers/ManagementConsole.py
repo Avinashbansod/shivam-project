972537082cf1873252feca31f29ba41cc21b1e1c
Avinash Bansod <avinash.bansod@kambi.com>
2022-03-25 13:59:53 +0100

    completed till the createGuard functionality

--- controllers/ManagementConsole.py
+++ controllers/ManagementConsole.py
@@ -1,17 +1,49 @@
 import sys
 from flask import render_template, redirect, url_for, request, abort
-from models.models import EmployeePersonalDetail , ServiceLocation
+from models.models import EmployeePersonalDetail, ServiceLocation
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
 
 def index():
     return render_template('management-console.html')
 
-def search_Guards():
+
+def searchGuards():
     employees = db.session.query(EmployeePersonalDetail.firstname, EmployeePersonalDetail.lastname,
-                   EmployeePersonalDetail.personaladdress, EmployeePersonalDetail.contactdetails, 
-                   ServiceLocation.city, ServiceLocation.serviceaddress) \
-                   .join(ServiceLocation) \
-                   .filter(EmployeePersonalDetail.empid == ServiceLocation.person_id).all()
-    
-    return render_template('search-guards.html',employees=employees)
+                                 EmployeePersonalDetail.personaladdress, EmployeePersonalDetail.contactdetails,
+                                 ServiceLocation.city, ServiceLocation.serviceaddress) \
+                                .join(ServiceLocation) \
+                                .filter(EmployeePersonalDetail.empid == ServiceLocation.person_id).all()
+
+    return render_template('search-guards.html', employees=employees)
+
+
+def renderCreateGuardForm():
+    return render_template('guard-registration-form.html')
+
+
+def createGuard():
+    data = request.form
+    isdetailsnotok = False
+    for key in data:
+        if (data.get(key) == None) or (data.get(key) == ''):
+            isdetailsnotok = True
+
+    if isdetailsnotok:
+        return render_template('guard-registration-form.html', status=f"Required fields are missing!")
+    else:
+        employee = EmployeePersonalDetail(data.get('firstname'), data.get('lastname'),
+                                          data.get('residentialaddress'), data.get('phonenumber'))
+        db.session.add(employee)
+        db.session.commit()
+        person_id = employee.empid
+        service_location = ServiceLocation(
+            data.get('servingcity'), data.get('servingaddress'), person_id)
+        db.session.add(service_location)
+        db.session.commit()
+        return render_template('guard-registration-form.html', status=f"{data.get('firstname')} {data.get('lastname')} is added to the database")
+
+
+def updateGuard():
+    pass
--- routes/management_console.py
+++ routes/management_console.py
@@ -1,8 +1,9 @@
 from flask import Blueprint
-from controllers.ManagementConsole import index , search_Guards
+from controllers.ManagementConsole import index , searchGuards, createGuard, renderCreateGuardForm, updateGuard
 management_console = Blueprint('management_console', __name__)
 management_console.route('/', methods=['GET'])(index)
-management_console.route('/search-guards', methods=['get'])(search_Guards)
-# management_console.route('/<int:user_id>', methods=['GET'])(show)
-# management_console.route('/<int:user_id>/edit', methods=['POST'])(update)
+management_console.route('/search-guards', methods=['get'])(searchGuards)
+management_console.route('/register-new-guard', methods=['POST'])(createGuard)
+management_console.route('/register-new-guard', methods=['GET'])(renderCreateGuardForm)
+management_console.route('/update-guard', methods=['GET'])(updateGuard)
 # management_console.route('/<int:user_id>', methods=['DELETE'])(destroy)
